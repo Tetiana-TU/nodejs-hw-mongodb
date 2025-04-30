97225cf91463f724b3206bdd152fa73bc7707712
Tetiana Dmytryienko <dmitrienko_tanya@ukr.net>
2025-04-30 10:14:17 +0300

    hw2

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import { readContacts } from './utils/readContacts.js';
-
-const start = async () => {
-  const contacts = await readContacts();
-  console.log('📋 Контакти:', contacts);
-};
-
-start();
