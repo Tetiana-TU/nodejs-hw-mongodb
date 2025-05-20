285be08d64f46c82f6ed50de678262d83999ed1d
Tetiana Dmytryienko <dmitrienko_tanya@ukr.net>
2025-05-20 13:40:04 +0300

    hw4

--- src/utils/calculatePaginationData.js
+++ src/utils/calculatePaginationData.js
@@ -1,14 +1,14 @@
 export const calculatePaginationData = ({ totalItems, perPage, page }) => {
   const totalPages = Math.ceil(totalItems / perPage);
-  const hasNextPage = page < totalPages;
   const hasPreviousPage = page > 1;
+  const hasNextPage = page < totalPages;
 
   return {
     page,
     perPage,
     totalItems,
     totalPages,
-    hasNextPage,
     hasPreviousPage,
+    hasNextPage,
   };
 };
