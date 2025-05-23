e5a8a427ef1585efb58852240652b1aa1be46cc5
Tetiana Dmytryienko <dmitrienko_tanya@ukr.net>
2025-05-23 14:26:13 +0300

    hw5

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 import contactsRouter from './routes/contactsRouter.js';
-
+import cookieParser from 'cookie-parser';
 const PORT = process.env.PORT || 3000;
 
 export const setupServer = () => {
@@ -33,4 +33,11 @@ export const setupServer = () => {
   app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
   });
+};
+export const startServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+  app.use(cookieParser());
 };
